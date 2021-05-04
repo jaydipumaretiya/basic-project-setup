527331af1120a9f5603007a6ada707d4ab5deae0
Jaydip Umaretiya <jaydip@enlistech.com>
2021-05-04 10:47:31 +0530

    Success Project done

--- app/src/main/java/app/basic/project/viewmodel/MainViewModel.kt
+++ app/src/main/java/app/basic/project/viewmodel/MainViewModel.kt
@@ -1,6 +1,5 @@
 package app.basic.project.viewmodel
 
-import android.app.Application
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.liveData
@@ -11,7 +10,6 @@ import kotlinx.coroutines.Dispatchers
 import java.io.IOException
 
 class MainViewModel(
-    app: Application,
     private val mainRepository: MainRepository
 ) : ViewModel() {
 
